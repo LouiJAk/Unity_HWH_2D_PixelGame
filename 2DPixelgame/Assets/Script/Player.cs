ebda91b97b9ad356bc739496dd26587a1e854e23
LouiJAk <71476978+LouiJAk@users.noreply.github.com>
2021-03-12 11:56:11 +0800

    程式宣告

--- 2DPixelgame/Assets/Script/Player.cs
+++ 2DPixelgame/Assets/Script/Player.cs
@@ -3,5 +3,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-   
+    public int lv = 1;
+    public float speed = 10.5f;
+    public bool isDead = false;
+    public string cName = "貓咪";
 }

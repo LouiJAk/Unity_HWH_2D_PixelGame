765464b9787ce0672637c8a6d092204a2b95bcc2
LouiJAk <71476978+LouiJAk@users.noreply.github.com>
2021-03-19 11:52:56 +0800

    玩家移動

--- 2DPixelgame/Assets/Script/Player.cs
+++ 2DPixelgame/Assets/Script/Player.cs
@@ -6,26 +6,32 @@ public class Player : MonoBehaviour
     [Header("等級")]
     public int lv = 1;
     [Header("移動速度"),Range(0,300)]
-    public float speed = 10.5f;
+    public float speed = 0.5f;
     [Header("角色是否死亡")]
     public bool isDead = false;
     [Header("角色名稱"),Tooltip("這是角色的名稱")]
     public string cName = "貓咪";
     [Header("虛擬搖桿")]
     public FixedJoystick joystick;
+    [Header("變形元件")]
+    public Transform tra;
+    [Header("動畫元件")]
+    public Animator ani;
 
     /// <summary>
     /// 移動
     /// </summary>
     private void Move()
     {
-        print("移動");
 
         float h = joystick.Horizontal;
-        print("水平:" + h);
-
         float v = joystick.Vertical;
-        print("垂直:" + v);
+
+        //變形元件.位移
+        tra.Translate( h * speed * Time.deltaTime , v * speed * Time.deltaTime ,0);
+
+        ani.SetFloat("水平", h);
+        ani.SetFloat("垂直", v);
 
     }
 

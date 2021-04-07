3044bbd8b18ef7b9033accb37e277ae4a4c330bf
LouiJAk <71476978+LouiJAk@users.noreply.github.com>
2021-04-07 19:38:03 +0800

    0326進度

--- 2DPixelgame/Assets/Script/CameraContral.cs
+++ 2DPixelgame/Assets/Script/CameraContral.cs
@@ -8,4 +8,44 @@ public class CameraContral : MonoBehaviour
     public Vector2 limitY = new Vector2(-5, 5);
     [Header("左右邊界")]
     public Vector2 limitX = new Vector2(-5, 5);
+    [Header("玩家")]
+    public Transform player;
+
+    //練習
+    /*public float a = 0;
+    public float b = 100;
+
+    public Vector3 v3a = new Vector3(0, 0, 0);
+    public Vector3 v3b = new Vector3(100, 100, 100);*/
+
+    private void Update()
+    {
+        /*//數學,差值(A,B,百分比)
+        a = Mathf.Lerp(a, b, 0.5f);
+        //三維向量,差值(A,B,百分比)
+        v3a = Vector3.Lerp(v3a, v3b, 0.5f);*/
+        Track();
+    }
+    /// <summary>
+    /// 追蹤
+    /// </summary>
+    private void Track()
+    {
+        Vector3 posCam = transform.position;   //取得 攝影機座標
+        Vector3 pospla = player.position;      //取得 玩家座標
+
+        posCam = Vector3.Lerp(posCam, pospla, 0.5f * speed * Time.deltaTime);
+        posCam.z = -10;                        //把Z軸放回-10
+
+        //Mathf.Clamp(要夾住的值, 最小值, 最大值)
+
+        posCam.x = Mathf.Clamp(posCam.x, limitX.x, limitX.y);
+        posCam.y = Mathf.Clamp(posCam.y, limitY.x, limitY.y);
+
+        transform.position = posCam;           //更新攝影機座標
+
+    }
+
+
+
 }
--- 2DPixelgame/Assets/Script/Player.cs
+++ 2DPixelgame/Assets/Script/Player.cs
@@ -17,6 +17,21 @@ public class Player : MonoBehaviour
     public Transform tra;
     [Header("動畫元件")]
     public Animator ani;
+    [Header("偵測範圍")]
+    public float rangeAttack = 1.2f;
+    [Header("音效來源")]
+    public AudioSource aud;
+    [Header("攻擊音效")]
+    public AudioClip soundAttack;
+
+    //事件:繪製圖示
+    private void OnDrawGizmos()
+    {
+        //指定圖示顏色(紅，綠，藍，透明度)
+        Gizmos.color = new Color(1, 0, 0, 0.2f);
+        //繪製圖示 球體(中心點，半徑)
+        Gizmos.DrawSphere(transform.position, rangeAttack);
+    }
 
     /// <summary>
     /// 移動
@@ -35,8 +50,19 @@ public class Player : MonoBehaviour
 
     }
 
-    private void Attack()
+    //要被按鈕呼叫必須設定為公開 public
+    public void Attack()
     {
+        //音效來源,播放一次(音效片段，音量)
+        aud.PlayOneShot(soundAttack, 0.5f);
+
+        //2D物理 圓形碰撞(中心點，半徑，方向，距離，圖層編號(1<<X))
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position, rangeAttack, -transform.up, 0, 1 << 8);
+
+        //如果 碰到的物件 標籤 為 道具 就刪除(碰到的碰撞器的遊戲物件)
+        if (hit.collider.tag == "道具") Destroy(hit.collider.gameObject);
+
+
 
     }
 

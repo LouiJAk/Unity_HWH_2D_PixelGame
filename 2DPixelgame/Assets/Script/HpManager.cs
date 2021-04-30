8f0bdb4dfd45ab8f5d8bf679e389b14eb2d93b9d
LouiJAk <71476978+LouiJAk@users.noreply.github.com>
2021-04-30 10:32:19 +0800

    敵人追蹤攻擊設定

---
+++ 2DPixelgame/Assets/Script/HpManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HpManager : MonoBehaviour
+{
+    [Header("血條")]
+    public Image bar;
+
+    /// <summary>
+    /// 輸入血量與血量最大值
+    /// </summary>
+    /// <param name="hp">當前血量</param>
+    /// <param name="hpMax">血量最大值</param>
+    public void UpdateHpBar(float hp, float hpMax)
+    {
+        
+    }
+}
